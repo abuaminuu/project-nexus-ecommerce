9d77f16e07d4db8025e9f19328f24d44423f0520
abuaminuu <i.abuaminu@gmail.com>
2026-01-26 12:59:04 +0100

    feat: correct working User and Product model

--- commerce/models.py
+++ commerce/models.py
@@ -30,62 +30,58 @@ class Product(models.Model):
         return self.name
     
 
-# class Order(models.Model):
-#     ORDERSTATUS = (
-#         ("pending", "Pending"),
-#         ("shipped", "Shipped"),
-#         ("delivered", "Delivered"),
-#         ("cancelled", "Cancelled")
-#     )
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     order_status= models.CharField(choices=ORDERSTATUS, null=False, default="pending", max_length=10)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Order(models.Model):
+    ORDERSTATUS = (
+        ("pending", "Pending"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+        ("cancelled", "Cancelled")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_status= models.CharField(choices=ORDERSTATUS, null=False, default="pending", max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return f"Order {self.id} by {self.user.username} status: {self.order_status}"
+    def total_amount(self):
+        return sum([items.price * items.quantity for items in self.items.all()])
 
-# class Payment(models.Model):
-#     PAYMENTSTATUS = (
-#         ("pending", "Pending"),
-#         ("cancel", "Cancel"),
-#         ("confirmed", "Confirmed")
-#     )
-# PAYMENTMETHOD = (
-# #         ("card", "Card"),
-# #         ("transfer", "Transfer"),
-# #         ("cash", "Cash")
-# #     )
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     payment_status= models.Choices(choices=STATUS, null=False, default="pending", max_length=10)
-#     payment_date = models.DateTimeField(auto_now_add=True)
-#     payment_method = models.CharField(max_length=50, default="Card")
-#     
-#       def total_amount(self):
-          # items = []
-        #   total = [item.price for item in self.order.product_set.all()]
-#         return self.amount + sum(total)
-#     def __str__(self):
-#         return f"Payment {self.id} user: {user.username} for Order {self.order.id} status: {payment_status}"
-#     class Meta:
-#         indexes = [
-#             models.Index(fields=['order', 'payment_date'], name='order_payment_date_idx'),
-#         ]
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username} status: {self.order_status}"
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
     price  = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
+
     def total_price(self):
         return self.quantity * self.price
+
+class Payment(models.Model):
+    STATUS = (
+        ("pending", "Pending"),
+        ("cancel", "Cancel"),
+        ("confirmed", "Confirmed")
+    )
+    
+    METHOD = (
+        ("card", "Card"),
+        ("transfer", "Transfer"),
+        ("cash", "Cash")
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status= models.Choices(choices=STATUS, null=False, default="pending", max_length=10)
+    method = models.CharField(max_length=16, choices=METHOD, default="card")
+    paid_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.id} user: {self.user.username} for Order {self.order.id} status: {self.status}"
