b2bac78a16a3fd87d28a46d7a87a72830a0f017c
abuaminuu <i.abuaminu@gmail.com>
2026-02-02 11:44:41 +0100

    feat: added urls file

--- commerce/urls.py
+++ commerce/urls.py
@@ -7,18 +7,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
 
-# add swagger docs: schema view config
-schema_view = get_schema_view(
-    openapi.Info(
-        title="E-commerce API",
-        default_version="v1",
-        description="API documentation for the E-commerce application",
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 router = DefaultRouter()
 
 router.register(r"users", views.USerViewSet, basename="users")
@@ -28,10 +16,11 @@ router.register(r"order_items", views.OrderItemViewSet, basename="order-items")
 router.register(r"payments", views.PaymentViewSet, basename="payments")
 
 urlpatterns = [
+
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    
+
     path("", include(router.urls)),
 ]
