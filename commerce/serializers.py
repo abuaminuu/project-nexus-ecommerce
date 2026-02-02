326cbba7c6f4fe338a33c41e1c43ce2d7e1fc97c
abuaminuu <i.abuaminu@gmail.com>
2026-02-02 11:44:07 +0100

    feat: added serializers file

---
+++ commerce/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from commerce.models import User, Product, Order, OrderItem, Payment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "is_staff"]
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'product_name', 'quantity', 'price', 'total_price']
+    
+    def get_total_price(self, object):
+        return object.total_price()
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_amount = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Order
+        fields = "__all__"
+    
+    def get_total_amount(self, object):
+        return object.total_amount()
+    
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
