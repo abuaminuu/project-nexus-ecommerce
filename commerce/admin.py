111c417105061fe0f16eda94e7c2034dc6862a16
abuaminuu <i.abuaminu@gmail.com>
2026-02-02 11:47:06 +0100

    feat: admin config

---
+++ commerce/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from commerce.models import User ,Product, Order, OrderItem, Payment
+def register(model):
+    return admin.site.register(model)
+
+# Register your models here.
+register(User)
+register(Product)
+register(Order)
+register(OrderItem)
+register(Payment)
+
+
