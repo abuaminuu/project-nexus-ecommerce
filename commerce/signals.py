a508a41f332dfc120707b622d01f14b170b92a38
abuaminuu <i.abuaminu@gmail.com>
2026-03-31 15:12:17 +0100

    feat: added commerce/signals file

---
+++ commerce/signals.py
@@ -0,0 +1,11 @@
+"""
+Docstring for signals
+this file contains all the signals for the commerce application
+1. sending welcome email for first time users
+2. order_completed: signal sent when an order is completed
+
+"""
+
+
+from django.dispatch import Signal
+
